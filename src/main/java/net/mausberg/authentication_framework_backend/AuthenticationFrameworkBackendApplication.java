e53bce3acb2a9f2c0507dcb3b52dbee41790814e
fmausberg <felix.mausberg@gmail.com>
2025-02-04 10:10:26 +0100

    Sucessfully created a jar

--- src/main/java/net/mausberg/authentication_framework_backend/AuthenticationFrameworkBackendApplication.java
+++ src/main/java/net/mausberg/authentication_framework_backend/AuthenticationFrameworkBackendApplication.java
@@ -2,8 +2,12 @@ package net.mausberg.authentication_framework_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "net.mausberg.authentication_framework_backend.repository")
+@EntityScan(basePackages = "net.mausberg.authentication_framework_backend.model")
 public class AuthenticationFrameworkBackendApplication {
 
 	public static void main(String[] args) {
