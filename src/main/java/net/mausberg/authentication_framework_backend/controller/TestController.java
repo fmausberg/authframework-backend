6a36ab64fd0e07d28279da7cb9641f27ccbd4379
fmausberg <felix.mausberg@gmail.com>
2025-02-17 14:48:06 +0100

    preparing pom & gitignore

--- src/main/java/net/mausberg/authentication_framework_backend/controller/TestController.java
+++ src/main/java/net/mausberg/authentication_framework_backend/controller/TestController.java
@@ -14,31 +14,44 @@ import org.slf4j.LoggerFactory;
 
 import net.mausberg.authentication_framework_backend.service.DatabaseService;
 
+/**
+ * TestController is a REST controller that provides endpoints for testing the backend and database connectivity.
+ */
 @RestController
 @RequestMapping("/api/v0/test")
 public class TestController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(TestController.class);
-	
-	@Autowired
-	private DatabaseService databaseService;
+    
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+    
+    @Autowired
+    private DatabaseService databaseService;
 
-	@GetMapping("/check")
-	public Map<String, String> checkConnection() {
-		logger.debug("\tFPM: TestController.checkConnection() started");
-		Map<String, String> status = new HashMap<>();
-		
-		status.put("backend", "available");
-		
-		// Check database availability
-		String databaseStatus = databaseService.isDatabaseAvailable() ? "available" : "not available";
-		status.put("database", databaseStatus);
+    /**
+     * Endpoint to check the availability of the backend and the database.
+     * 
+     * @return a map containing the status of the backend and the database.
+     */
+    @GetMapping("/check")
+    public Map<String, String> checkConnection() {
+        logger.debug("\tFPM: TestController.checkConnection() started");
+        Map<String, String> status = new HashMap<>();
+        
+        status.put("backend", "available");
+        
+        // Check database availability
+        String databaseStatus = databaseService.isDatabaseAvailable() ? "available" : "not available";
+        status.put("database", databaseStatus);
 
-		return status;
-	}
-	
-	@GetMapping("/public")
-	public @ResponseBody String getPublicInformation() {
-		return "public Information";
-	}
+        return status;
+    }
+    
+    /**
+     * Endpoint to get public information.
+     * 
+     * @return a string containing public information.
+     */
+    @GetMapping("/public")
+    public @ResponseBody String getPublicInformation() {
+        return "public Information";
+    }
 }
