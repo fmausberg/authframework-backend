fe1e22182cc9bce4a47a768a913ea0195110b6e8
fmausberg <felix.mausberg@gmail.com>
2025-03-06 10:03:46 +0100

    professionalized it - but JPA will still not load if database is not avaliable - server won't start then

--- src/main/java/net/mausberg/authentication_framework_backend/controller/GlobalExceptionHandler.java
+++ src/main/java/net/mausberg/authentication_framework_backend/controller/GlobalExceptionHandler.java
@@ -9,57 +9,68 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.mail.MessagingException;
 import net.mausberg.authentication_framework_backend.config.JwtTokenExpiredException;
+import net.mausberg.authentication_framework_backend.exception.DatabaseConnectionException;
 import net.mausberg.authentication_framework_backend.model.*;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-	
-	private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
+    
+    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
 
-	@ExceptionHandler(IllegalArgumentException.class)
-	public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
-		String errorMessage = "Error: " + ex.getMessage();
-		logger.error(errorMessage);
-		return new ResponseEntity<>(
-				new ErrorResponse(errorMessage),
-				HttpStatus.BAD_REQUEST);
-	}
-	
-	@ExceptionHandler(MessagingException.class)
-	public ResponseEntity<ErrorResponse> handleMessagingException(MessagingException e) {
-		String errorMessage = "Email sending error: " + e.getMessage();
-		logger.error(errorMessage);
-		return new ResponseEntity<>(
-				new ErrorResponse(errorMessage),
-				HttpStatus.INTERNAL_SERVER_ERROR);
-	}
-	
-	@ExceptionHandler(JwtTokenExpiredException.class)
-	public ResponseEntity<ErrorResponse> handleJwtTokenExpiredException(ExpiredJwtException ex) {
-		String errorMessage = "JWT token has expired. Please login again.(caught with Spring MVC Exception)";
-		logger.error(errorMessage);
-		return new ResponseEntity<>(
-				new ErrorResponse(errorMessage),
-				HttpStatus.UNAUTHORIZED);
-	}
-	
-	@ExceptionHandler(ExpiredJwtException.class)
-	public ResponseEntity<ErrorResponse> handleExpiredJwtException(ExpiredJwtException ex) {
-		String errorMessage = "JWT token has expired. Please login again. (caught with JWT Exception)";
-		logger.error(errorMessage);
-		return new ResponseEntity<>(
-				new ErrorResponse(errorMessage),
-				HttpStatus.UNAUTHORIZED);
-	}
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+        String errorMessage = "Error: " + ex.getMessage();
+        logger.error(errorMessage);
+        return new ResponseEntity<>(
+			new ErrorResponse(errorMessage),
+			HttpStatus.BAD_REQUEST);
+    }
 
-	@ExceptionHandler(Exception.class)
-	public ResponseEntity<ErrorResponse> handleGeneralException(Exception ex) {
-		String errorMessage = "An unexpected error occurred: " + ex.getMessage();
-		logger.error(errorMessage);
-		return new ResponseEntity<>(
-				new ErrorResponse(errorMessage),
-				HttpStatus.INTERNAL_SERVER_ERROR);
-	}
+    @ExceptionHandler(MessagingException.class)
+    public ResponseEntity<ErrorResponse> handleMessagingException(MessagingException ex) {
+        String errorMessage = "Email sending error: " + ex.getMessage();
+        logger.error(errorMessage);
+        return new ResponseEntity<>(
+			new ErrorResponse(errorMessage),
+			HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(JwtTokenExpiredException.class)
+    public ResponseEntity<ErrorResponse> handleJwtTokenExpiredException(JwtTokenExpiredException ex) {
+        String errorMessage = "JWT token has expired. Please login again. (caught with Spring MVC Exception)";
+        logger.error(errorMessage);
+        return new ResponseEntity<>(
+			new ErrorResponse(errorMessage),
+			HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<ErrorResponse> handleExpiredJwtException(ExpiredJwtException ex) {
+        String errorMessage = "JWT token has expired. Please login again. (caught with JWT Exception)";
+        logger.error(errorMessage);
+        return new ResponseEntity<>(
+			new ErrorResponse(errorMessage),
+			HttpStatus.UNAUTHORIZED);
+    }
+
+	@ExceptionHandler(DatabaseConnectionException.class)
+    public ResponseEntity<ErrorResponse> handleDatabaseConnectionException(DatabaseConnectionException ex) {
+        String errorMessage = "Database is not reachable: " + ex.getMessage();
+        logger.error(errorMessage);
+        return new ResponseEntity<>(
+			new ErrorResponse(errorMessage),
+			HttpStatus.SERVICE_UNAVAILABLE);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGeneralException(Exception ex) {
+        String errorMessage = "An unexpected error occurred: " + ex.getMessage();
+        logger.error(errorMessage);
+        return new ResponseEntity<>(
+			new ErrorResponse(errorMessage),
+			HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
